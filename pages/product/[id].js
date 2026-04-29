84b7279c659aee9751c50d0057a90fafc40fed5e
gumustasas <144227647+gumustasas@users.noreply.github.com>
2026-04-29 22:23:06 +0300

    Update [id].js

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -36,31 +36,42 @@ export default function Product() {
   return (
     <div className="bg-gray-100 min-h-screen p-4">
 
+      {/* RESİM */}
       <img
         src={product.image}
         className="w-full h-64 object-cover rounded bg-gray-200"
+        onError={(e) => {
+          e.currentTarget.src =
+            "https://images.unsplash.com/photo-1581091215367-59ab6b7b9d7b"
+        }}
       />
 
+      {/* BAŞLIK */}
       <h1 className="text-xl font-bold mt-3">
         {product.name}
       </h1>
 
+      {/* PUAN */}
       <div className="text-yellow-500 mt-1">
         ⭐ {product.rating} ({product.reviews})
       </div>
 
+      {/* FİYAT */}
       <p className="text-orange-600 text-xl font-bold mt-2">
         {product.price} TL
       </p>
 
+      {/* KARGO */}
       <p className="text-green-600">
         {product.cargo}
       </p>
 
+      {/* BUTON */}
       <button className="bg-orange-500 text-white w-full mt-4 p-3 rounded">
         Sepete Ekle
       </button>
 
+      {/* AÇIKLAMA */}
       <h2 className="mt-4 font-bold">Ürün Açıklaması</h2>
       <p>{product.description}</p>
 
