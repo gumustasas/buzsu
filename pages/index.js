bc2c588c9af6bfc6f30c26971e4472f1b92fda2b
gumustasas <144227647+gumustasas@users.noreply.github.com>
2026-04-29 22:32:58 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
       id: 1,
       name: "Buzsu 6 Aşamalı Filtre Seti",
       price: 1699,
-      image: "https://picsum.photos/400/300?random=1",
+      image: "https://picsum.photos/400/300?random=11",
     },
     {
       id: 2,
       name: "LG Chem 5 Filtre",
       price: 1299,
-      image: "https://picsum.photos/400/300?random=2",
+      image: "https://picsum.photos/400/300?random=12",
     }
   ];
 
@@ -36,30 +36,39 @@ export default function Home() {
         %10 İndirim Fırsatı!
       </div>
 
-      {/* PRODUCT GRID */}
+      {/* GRID */}
       <div className="grid grid-cols-2 gap-4 p-4">
 
         {products.map((p) => (
           <Link key={p.id} href={`/product/${p.id}`}>
 
-            <div className="bg-white p-3 rounded shadow">
+            <div className="bg-white rounded shadow flex flex-col h-full">
 
-              <img
-                src={p.image}
-                className="w-full h-40 object-cover rounded"
-              />
+              {/* RESİM ALANI */}
+              <div className="h-40 flex items-center justify-center bg-gray-100 rounded-t">
+                <img
+                  src={p.image}
+                  className="max-h-full object-contain"
+                />
+              </div>
 
-              <h2 className="font-bold mt-2">
-                {p.name}
-              </h2>
+              {/* İÇERİK */}
+              <div className="p-3 flex flex-col flex-grow">
 
-              <p className="text-orange-500 font-bold text-lg">
-                {p.price} TL
-              </p>
+                <h2 className="font-bold min-h-[48px]">
+                  {p.name}
+                </h2>
 
-              <button className="bg-orange-500 text-white w-full py-2 mt-2 rounded">
-                Sepete Ekle
-              </button>
+                <p className="text-orange-500 font-bold text-lg mt-2">
+                  {p.price} TL
+                </p>
+
+                {/* BUTON HER ZAMAN ALTA SABİT */}
+                <button className="bg-orange-500 text-white w-full py-2 mt-auto rounded">
+                  Sepete Ekle
+                </button>
+
+              </div>
 
             </div>
 
